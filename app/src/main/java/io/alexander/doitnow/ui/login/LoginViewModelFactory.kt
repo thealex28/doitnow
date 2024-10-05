294b29b363b8783981eaefcd44f74794cba2d41e
Alexander <hialexsam@gmail.com>
2024-10-05 19:51:39 +0200

    added login activity

---
+++ app/src/main/java/io/alexander/doitnow/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package io.alexander.doitnow.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import io.alexander.doitnow.data.LoginDataSource
+import io.alexander.doitnow.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                loginRepository = LoginRepository(
+                    dataSource = LoginDataSource()
+                )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
